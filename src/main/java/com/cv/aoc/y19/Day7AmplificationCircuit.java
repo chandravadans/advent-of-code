3dd0bb50edb10f7cbbc7da7390f6429e440185b4
Chandravadan <s.chandravadan@gmail.com>
2019-12-14 08:26:07 +1100

    December 7, 2019

---
+++ src/main/java/com/cv/aoc/y19/Day7AmplificationCircuit.java
@@ -0,0 +1,175 @@
+package com.cv.aoc.y19;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.Integer.MIN_VALUE;
+import static java.lang.Math.max;
+
+public class Day7AmplificationCircuit {
+    public static void main(String[] args) {
+        assert Day7Solver.solve("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0") == 43210;
+        assert Day7Solver.solve("3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0") == 54321;
+        assert Day7Solver.solve("3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0") == 65210;
+        assert Day7Solver.solve("3,8,1001,8,10,8,105,1,0,0,21,38,47,72,97,122,203,284,365,446,99999,3,9,1001,9,3,9,1002,9,5,9,1001,9,4,9,4,9,99,3,9,102,3,9,9,4,9,99,3,9,1001,9,2,9,102,5,9,9,101,3,9,9,1002,9,5,9,101,4,9,9,4,9,99,3,9,101,5,9,9,1002,9,3,9,101,2,9,9,102,3,9,9,1001,9,2,9,4,9,99,3,9,101,3,9,9,102,2,9,9,1001,9,4,9,1002,9,2,9,101,2,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,99") == 95757;
+    }
+}
+
+class Day7Solver {
+
+    static int solve(String program) {
+        List<Integer> instructions = Arrays.stream(program.split(",")).map(Integer::parseInt).collect(Collectors.toList());
+        int maxOutput = MIN_VALUE;
+        List<Integer> maxConfig = new ArrayList<>();
+        for (int a = 0; a < 5; a++) {
+            for (int b = 0; b < 5; b++) {
+                for (int c = 0; c < 5; c++) {
+                    for (int d = 0; d < 5; d++) {
+                        for (int e = 0; e < 5; e++) {
+                            if (allUnique(Arrays.asList(a, b, c, d, e))) {
+                                int result = run(instructions, Arrays.asList(a, b, c, d, e), 0);
+                                maxOutput = max(maxOutput, result);
+                                maxConfig = Arrays.asList(a, b, c, d, e);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("Max config = " + maxConfig.stream().map(String::valueOf).collect(Collectors.joining(",")));
+        System.out.println("Max output = " + maxOutput);
+        return maxOutput;
+    }
+
+    static int solve2(String program) {
+        List<Integer> instructions = Arrays.stream(program.split(",")).map(Integer::parseInt).collect(Collectors.toList());
+        int maxOutput = MIN_VALUE;
+        List<Integer> maxConfig = new ArrayList<>();
+        for (int a = 0; a < 5; a++) {
+            for (int b = 0; b < 6; b++) {
+                for (int c = 0; c < 7; c++) {
+                    for (int d = 0; d < 8; d++) {
+                        for (int e = 0; e < 9; e++) {
+                            if (allUnique(Arrays.asList(a, b, c, d, e))) {
+                                int result = run(instructions, Arrays.asList(a, b, c, d, e), 0);
+                                maxOutput = max(maxOutput, result);
+                                maxConfig = Arrays.asList(a, b, c, d, e);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("Max config = " + maxConfig.stream().map(String::valueOf).collect(Collectors.joining(",")));
+        System.out.println("Max output = " + maxOutput);
+        return maxOutput;
+    }
+
+
+    private static int run(List<Integer> instructions, List<Integer> phaseSettings, int kickstart) {
+        System.out.println("Running for phase settings " + phaseSettings.stream().map(String::valueOf).collect(Collectors.joining(",")));
+        List<Integer> ops = new ArrayList<>();
+        List<IntCodeComputer7> computers = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            computers.add(new IntCodeComputer7(instructions));
+        }
+        for (int i = 0; i < 5; i++) {
+            ops.add(computers.get(i).executeProgram(Arrays.asList(phaseSettings.get(i), i == 0 ? kickstart : ops.get(i - 1))).get(0));
+            System.out.println("i : " + i + " output: " + ops.get(i));
+        }
+        return ops.get(4);
+    }
+
+    private static boolean allUnique(List<Integer> integers) {
+        int lo = Collections.min(integers);
+        int[] hash = new int[integers.size()];
+        integers.forEach(i -> hash[i - lo]++);
+        for (int value : hash) {
+            if (value != 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class IntCodeComputer7 {
+    int inputPointer;
+    private List<Integer> memory;
+    private List<Integer> outputs;
+    private List<Integer> inputs;
+
+    public IntCodeComputer7(List<Integer> memory) {
+        this.memory = memory;
+        outputs = new ArrayList<>();
+    }
+
+    public List<Integer> executeProgram(List<Integer> inputs) {
+        this.inputs = inputs;
+        int pc = 0;
+        int delta = 4;
+        while (delta != MIN_VALUE) {
+            delta = processOperation(pc);
+            pc += delta;
+        }
+        return getOutputs();
+    }
+
+    private int processOperation(int pc) {
+        if (memory.get(pc) == 99) {
+            return MIN_VALUE;
+        }
+        String instructionOpCode = String.valueOf(memory.get(pc));
+        int operation = memory.get(pc) > 99 ? Integer.parseInt(instructionOpCode.substring(instructionOpCode.length() - 2)) : memory.get(pc);
+        switch (operation) {
+            case 1:
+            case 2:
+                int[] addMulParams = extractParameters(pc, 2);
+                memory.set(memory.get(pc + 3), operation == 1 ? addMulParams[0] + addMulParams[1] : addMulParams[0] * addMulParams[1]);
+                return 4;
+            case 3:
+                memory.set(memory.get(pc + 1), inputs.get(inputPointer++));
+                System.out.println("Read input #" + (inputPointer - 1));
+                return 2;
+            case 4:
+                outputs.add(memory.get(memory.get(pc + 1)));
+                return 2;
+            case 5:
+                int[] jitParams = extractParameters(pc, 2);
+                return jitParams[0] != 0 ? jitParams[1] - pc : 3;
+            case 6:
+                int[] jifParams = extractParameters(pc, 2);
+                return jifParams[0] == 0 ? jifParams[1] - pc : 3;
+            case 7:
+                int[] ltParams = extractParameters(pc, 2);
+                memory.set(memory.get(pc + 3), ltParams[0] < ltParams[1] ? 1 : 0);
+                return 4;
+            case 8:
+                int[] eqParams = extractParameters(pc, 2);
+                memory.set(memory.get(pc + 3), eqParams[0] == eqParams[1] ? 1 : 0);
+                return 4;
+            default:
+                throw new RuntimeException("Encountered unknown opcode " + memory.get(pc));
+        }
+    }
+
+    private int[] extractParameters(int pc, int numParams) {
+        String instructionOpCode = String.valueOf(memory.get(pc));
+        String parameterModes = instructionOpCode.length() > 2 ? instructionOpCode.substring(0, instructionOpCode.length() - 2) : "";
+        while (parameterModes.length() < numParams) {
+            parameterModes = "0".concat(parameterModes);
+        }
+        int[] result = new int[numParams];
+        for (int i = 0; i < numParams; i++) {
+            result[i] = parameterModes.charAt(numParams - 1 - i) == '0' ? memory.get(memory.get(pc + 1 + i)) : memory.get(pc + 1 + i);
+        }
+        return result;
+    }
+
+    public List<Integer> getOutputs() {
+        return outputs;
+    }
+}
